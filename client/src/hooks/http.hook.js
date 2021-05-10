80df6850d71e06c4f0cb41c5eaf92edae9a3e543
yulya-buraya <buraya.yulya@gmail.com>
2021-05-10 19:39:33 +0300

    ver-18-not-work

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -4,10 +4,11 @@ export const useHttp = () => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
+
     const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
         console.log('вызов колбэка')
-        setLoading(true)
-        try {
+/*         setLoading(true)
+ */        try {
             if (body) {
                 body = JSON.stringify(body)
                 headers['Content-Type'] = 'application/json'
@@ -20,12 +21,12 @@ export const useHttp = () => {
                 throw new Error(data.message || 'Что-то пошло не так')
             }
 
-            setLoading(false)
-
+/*             setLoading(false)
+ */
             return data
         } catch (e) {
-            setLoading(false)
-            setError(e.message)
+/*             setLoading(false)
+ */            setError(e.message)
             throw e
         }
     }, [])
--- client/src/pages/UserList.js
+++ client/src/pages/UserList.js
@@ -1,73 +1,72 @@
 import React, { useState } from "react"
 import { DeleteUserForm } from "../components/forms/DeleteUserForm"
-/* import { useHttp } from '../hooks/http.hook'
- */
+import { useHttp } from "../hooks/http.hook";
+
 export const UserList = ({ users, setUsers }) => {
 
-    const [isDeleteUserFormActive, setDeleteUserFormActive] = useState(false)
-    const deleteUserInfoHandler = (id) => {
-            let index = users.findIndex((u) => u.user[0]._id === id)
-            setUsers((prevUsers) => prevUsers.filter((u, i) => i != index))
-    }  
-    if (!users.lenght == 0) {
+    const { request } = useHttp()
+    const [form, setForm] = useState(null)
+
+    const deleteUser = async (id) => {
+        await request('api/user/delete', 'DELETE', { id })
+        let index = users.findIndex((u) => u.user[0]._id === id)
+        setUsers((prevUsers) => prevUsers.filter((u, i) => i != index))
+        setForm(null)
+    }
+
+    const showForm = (user) => {
+        setForm(<DeleteUserForm setForm={setForm} user={user} deleteUser={deleteUser}/>)
+    }
+
+    if (!users.lenght == null) {
         return (
             <>
-                <img className="not-found-icons" src="image/not-found.jpg" />
+                <img className="not-found-icons" src="image/not-found.jpg"/>
                 <p className="text-align-center">Данные не найдены!</p>
             </>
         )
-
     }
-    else {
-        return (
-            <div className='users-content-block'>
-                <table id="users">
-                    <tbody>
-                        <tr>
-                            <th>№</th>
-                            <th>Email</th>
-                            <th>Имя и Фамилия</th>
-                            <th>Возраст, лет</th>
-                            <th>Рост, см</th>
-                            <th>Вес, кг</th>
-                            <th>Активность</th>
-                            <th>Цель</th>
-                            <th>Удалить</th>
 
+    return (
+        <div className='users-content-block'>
+            <table id="users">
+                <tbody>
+                <tr>
+                    <th>№</th>
+                    <th>Email</th>
+                    <th>Имя и Фамилия</th>
+                    <th>Возраст, лет</th>
+                    <th>Рост, см</th>
+                    <th>Вес, кг</th>
+                    <th>Активность</th>
+                    <th>Цель</th>
+                    <th>Удалить</th>
+
+                </tr>
+                {users.map((user, index) => {
+                    return (
+                        <tr key={user._id}>
+                            <td>{index + 1}</td>
+                            <td>{user.user[0].email}</td>
+                            <td>{user.firstname} {user.lastname}</td>
+                            <td>{user.age}</td>
+                            <td>{user.height}</td>
+                            <td>{user.weight}</td>
+                            <td>{user.activity}</td>
+                            <td>{user.purpose}</td>
+                            <td>
+                                <img
+                                    className="delete-icons"
+                                    src="image/trash-bin.svg"
+                                    onClick={() => showForm(user.user[0])}
+                                />
+                            </td>
                         </tr>
-                        {users.map((user, index) => {
-                            return (
-                                <tr key={user._id}>
-                                    <td>{index + 1}</td>
-                                    <td>{user.user[0].email}</td>
-                                    <td>{user.firstname} {user.lastname}</td>
-                                    <td>{user.age}</td>
-                                    <td>{user.height}</td>
-                                    <td>{user.weight}</td>
-                                    <td>{user.activity}</td>
-                                    <td>{user.purpose}</td>
-                                    <td>
-                                        <img
-                                            className="delete-icons"
-                                            src="image/trash-bin.svg"
-                                            onClick={() => setDeleteUserFormActive(true)}
-                                        />
-                                    </td>
-                                    {isDeleteUserFormActive
-                                            && <DeleteUserForm
-                                            active={isDeleteUserFormActive}
-                                            setActive={setDeleteUserFormActive}
-                                            user={user.user[0]}
-                                        />
-                                    }
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-                {console.log("userlist", isDeleteUserFormActive)}
-
-            </div>
-        )
-    }
+                    )
+                })}
+                </tbody>
+            </table>
+            {form}
+        </div>
+    )
 }
