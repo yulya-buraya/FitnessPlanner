d6524a8fdd41956c1685f564d8ade2e33b4a8fb1
Alexei Tsagoyko <alexei.czagojko@mail.ru>
2021-05-29 11:08:22 +0300

    v32

--- client/src/components/MealPlanner/mealPlannerComponents/SubmenuContentMealPlans.js
+++ client/src/components/MealPlanner/mealPlannerComponents/SubmenuContentMealPlans.js
@@ -1,16 +1,54 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "../../../styles/training.css"
-export const SubmenuContentMealPlans = ({setMealPlans}) => {
+
+function filterMealPlans(value, mealPlans) {
+    return mealPlans.filter(exercise => {
+        if (exercise.name.includes(value)) {
+            return true;
+        }
+        return false;
+    })
+}
+
+export const SubmenuContentMealPlans = ({ mealPlans, setMealPlans }) => {
+    const buffer = useRef({ settled: false, data: null });
+    const searchInput = useRef(null);
+
+    const searchMealPlans = () => {
+        let value = searchInput.current.value;
+
+        if (value == '') {
+            return setMealPlans(buffer.current.data);
+        }
+
+        let findExercises = filterMealPlans(value, buffer.current.data);
+        setMealPlans(findExercises)
+    };
+
+    useEffect(() => {
+        if (!buffer.current.settled) {
+            buffer.current = {
+                data: mealPlans,
+                settled: mealPlans?.length ? true : false
+            };
+        }
+    }, [mealPlans]);
 
     return (
         <div className="submenu-food">
             <div className="search-input-block">
-                <input type="text" placeholder="Искать здесь..."/>
+                <input
+                    type="text"
+                    placeholder="Искать здесь..."
+                    ref={searchInput}
+                    onKeyDown={(e) => {
+                        if (e.keyCode === 13) searchMealPlans();
+                    }}/>
                 <button type="submit">
                     <i className="fa fa-search"></i>
                 </button>
             </div>
-      
+
         </div>
     );
 }
--- client/src/components/TrainingPlanning/workoutComponents/FilterWorkoutBlock.js
+++ client/src/components/TrainingPlanning/workoutComponents/FilterWorkoutBlock.js
@@ -1,46 +1,108 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import "../../../styles/training.css"
 
-export const FilterWorkoutBlock = () => {
+export const FilterWorkoutBlock = ({ workouts, setWorkouts }) => {
+    const buffer = useRef({});
+
+    const filterWorkout = (e) => {
+        const { props, data } = buffer.current;
+        props[e.target.name] = e.target.value;
+        console.log(buffer.current);
+
+        let _workouts = data.filter((workout) => (
+            workout.purpose === props.purpose
+            && workout.gender === props.gender
+        ));
+
+        return setWorkouts(_workouts);
+    };
+
+    useEffect(() => {
+        console.log('in filter', workouts);
+        if (!buffer.current.settled) {
+            buffer.current = {
+                data: workouts,
+                settled: workouts?.length ? true : false,
+                props: { purpose: 'Похудение', gender: 'Универсальная' }
+            };
+        }
+    }, [workouts]);
+
     return (
-                  <div className="filter-form-training">
-                    <div className="header-select-form">ПОДОБРАТЬ ПЛАН ТРЕНИРОВОК:</div>
-                    <hr className="hr-for-table" />
-                    <br />
-                    <div className="purpose-block">
-                        <p className='text-for-form'>Какова ваша цель?</p>
-                        <div className="radio-purpose-button">
-                            <label>
-                                <input name="purpose" /* onChange={changeHandler} */ value='Похудеть' type="radio" type="radio" /* checked={form.gender == 'мужской'?true:false} */ />
-                                <span>Похудеть</span>
-                            </label>
-                            <label>
-                                <input name="purpose" /* onChange={changeHandler} */ value='Набрать вес' type="radio" type="radio" /* checked={form.gender == 'женский'?true:false} */ />
-                                <span>Набрать вес</span>
-                            </label>
-                            <label>
-                                <input name="purpose" /* onChange={changeHandler} */ value='Выглядеть спортивно' type="radio" type="radio" /* checked={form.gender == 'женский'?true:false} */ />
-                                <span>Выглядеть спортивно</span>
-                            </label>
-                        </div>
+        <div className="filter-form-training">
+            <div className="header-select-form">ПОДОБРАТЬ ПЛАН ТРЕНИРОВОК:</div>
+            <div className="filter-form-training-container">
+                <div className="purpose-block">
+                    <p className='text-for-form'>Какова ваша цель?</p>
+                    <div className="radio-purpose-button">
+                        <label>
+                            <input
+                                name="purpose"
+                                value='Похудение'
+                                type="radio"
+                                onChange={filterWorkout}
+                            />
+                            <span>Похудение</span>
+                        </label>
+                        <label>
+                            <input
+                                name="purpose"
+                                value='Набор мышечной массы'
+                                type="radio"
+                                onChange={filterWorkout}
+                            />
+                            <span>Набрать вес</span>
+                        </label>
+                        <label>
+                            <input
+                                name="purpose"
+                                value='Поддержание формы'
+                                type="radio"
+                                onChange={filterWorkout}
+                            />
+                            <span>Выглядеть спортивно</span>
+                        </label>
                     </div>
-                    <div className="equipment-block">
-                        <p className='text-for-form'>С использованием инвентаря?</p>
-                        <div className="radio-equipment-button">
-                            <label>
-                                <input name="equipment" /* onChange={changeHandler} */ value='да' type="radio" type="radio" /* checked={form.gender == 'мужской'?true:false} */ />
-                                <span>Да</span>
-                            </label>
-                            <label>
-                                <input name="equipment" /* onChange={changeHandler} */ value='нет' type="radio" type="radio" /* checked={form.gender == 'женский'?true:false} */ />
-                                <span>Нет</span>
-                            </label>
-                        </div>
-                    </div>
-                    <button className="select-btn"
-                        id="selectTraining">
-                        ПОДОБРАТЬ
-                </button>
                 </div>
+                <div className="equipment-block">
+                    <p className='text-for-form'>Для кого тренировка?</p>
+                    <div className="radio-equipment-button">
+                        <label>
+                            <input
+                                name="gender"
+                                value="Для женщин"
+                                type="radio"
+                                onChange={filterWorkout}
+                            />
+                            <span>Для женщин</span>
+                        </label>
+                        <label>
+                            <input
+                                name="gender"
+                                value="Для мужчин"
+                                type="radio"
+                                onChange={filterWorkout}
+                            />
+                            <span>Для мужчин</span>
+                        </label>
+                        <label>
+                            <input
+                                name="gender"
+                                value="Универсальная"
+                                type="radio"
+                                onChange={filterWorkout}
+                            />
+                            <span>Универсальная</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <button
+                className="filter-btn"
+                id="selectTraining"
+            >
+                ПОДОБРАТЬ
+            </button>
+        </div>
     );
 }
